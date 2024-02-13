a27c0af5a51df38e908e9bb9d02b61f89d12012b
root <root@BettysHp>
2024-02-13 23:51:34 +0300

    sol

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-""" A function that queries the Reddit API and returns the number of subscribers.
+""" A function that queries the Reddit API and returns
+the number of subscribers.
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
     headers = {
-            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Apple'+
-            'WebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Apple' +
+            'WebKit/537.36 (KHTML, like Gecko)Chrome/121.0.0.0 Safari/537.36'
     }
     r = requests.get('https://www.reddit.com/r/{:}/about.json'.format(
         subreddit), headers=headers, allow_redirects=False)
