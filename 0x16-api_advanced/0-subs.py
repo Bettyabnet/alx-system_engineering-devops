e230ece09daadb8300f316149430f17c7d990f84
root <root@BettysHp>
2024-02-14 21:48:07 +0300

    some

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,8 +11,11 @@ def number_of_subscribers(subreddit):
             'WebKit/537.36 (KHTML, like Gecko)Chrome/121.0.0.0 Safari/537.36'
     }
     r = requests.get('https://www.reddit.com/r/{:}/about.json'.format(
-        subreddit), headers=headers, allow_redirects=False)
-    if r.status_code >= 300:
+        subreddit), headers=headers, allow_redirects=True)
+    # Check the final URL of the response
+    final_url = r.url
+    expected_url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    if final_url != expected_url:
         return 0
     json = r.json()
     data_dict = json.get('data')
