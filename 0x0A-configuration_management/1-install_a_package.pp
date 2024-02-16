75c704e57c4246e67f3a828a4cc7fbf7c74b7b18
root <root@eliana>
2024-02-16 13:27:28 +0300

    some changes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # install flask from pip3.
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_flask':
+  command => 'pip3 install flask==2.1.0',
+  path    => ['/usr/bin/'],
+  unless  => 'pip3 show flask | grep -q "Version: 2.1.0"',
 }
