67fe93d34f517b3188352102fccb148001b86044
root <root@eliana>
2024-02-16 13:14:43 +0300

    commitmtnt

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 # Execute the command 'pkill killmenow'
-exec { 'killmenow': returns => [0, 1],
+exec { 'killmenow':
+  returns => [0, 1],
   command => 'pkill killmenow',
   path    => '/bin:/usr/bin',
 }
